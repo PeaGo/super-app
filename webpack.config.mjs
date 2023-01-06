1a861a77a51cffe59f1e3f0d0bacefd7a4783e49
tiendn <dongoctien97@gmail.com>
2023-01-06 11:23:49 +0700

    bundle

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -249,6 +249,7 @@ export default env => {
 
       new Repack.plugins.ModuleFederationPlugin({
         name: 'miniapp',
+        
         exposes: {
           './App': './src/App.js',
         },
